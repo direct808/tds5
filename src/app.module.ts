090b8960d47b069e0a8fbea1b09d173a8f9d998c
Larin Sergey <LarinSS@fsk.ru>
2025-04-06 19:45:52 +0300

    Create source

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
 import { SourceModule } from './source'
 import { AppConfigModule, AppDbModule } from './config'
+import { APP_INTERCEPTOR } from '@nestjs/core'
+import { StartRequestInterceptor } from './start-request.interceptor'
 
 @Module({
   imports: [AppConfigModule, AppDbModule, SourceModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: StartRequestInterceptor,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/start-request.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common'
+import { finalize } from 'rxjs'
+
+@Injectable()
+export class StartRequestInterceptor implements NestInterceptor {
+  private readonly logger = new Logger('StartRequestInterceptor')
+
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const now = Date.now()
+    const name = context.getHandler().name
+    this.logger.log(`Start request ${name}`)
+
+    return next.handle().pipe(
+      finalize(() => {
+        const duration = Date.now() - now
+        this.logger.log(`End request ${name}, duration: ${duration}`)
+      }),
+    )
+  }
+}
