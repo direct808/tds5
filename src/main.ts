590c57a512f1baa59dc5d39a0bac9c64c746c225
Larin Sergey <direct808@yandex.ru>
2025-04-06 19:07:48 +0300

    MW

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.setGlobalPrefix('api')
+  app.useGlobalPipes(new ValidationPipe())
+
+  const config = new DocumentBuilder()
+    .setTitle('TDS 5')
+    .setDescription('Трекер веб-аналитики и отслеживания траффика')
+    .setVersion('1.0')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory, { swaggerOptions: {} })
+
+  await app.listen(3300)
 }
-bootstrap();
+
+void bootstrap()
