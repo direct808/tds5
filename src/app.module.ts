775ea94ee8b4144d863a8b19ac589afe17697e9a
Larin Sergey <LarinSS@fsk.ru>
2025-04-06 19:35:35 +0300

    Typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { SourceModule } from './source'
+import { AppConfigModule, AppDbModule } from './config'
 
 @Module({
-  imports: [SourceModule],
+  imports: [AppConfigModule, AppDbModule, SourceModule],
 })
 export class AppModule {}
