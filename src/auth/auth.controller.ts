d9ee356c2cca6c8f2b8bff8b8f833050b99e682f
Larin Sergey <direct808@yandex.ru>
2025-04-07 13:54:01 +0300

    Refactoring

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,14 @@ import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { LoginDto } from './dto'
 import { LoginRequest, SkipAuth } from './types'
-import { JwtAuthGuard } from './local-auth.guard'
+import { LocalAuthGuard } from './guards'
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @SkipAuth()
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Body() loginDto: LoginDto, @Req() req: LoginRequest) {
     return this.authService.sign(req.user)
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,8 @@
 import { ExtractJwt, Strategy } from 'passport-jwt'
 import { PassportStrategy } from '@nestjs/passport'
 import { Injectable } from '@nestjs/common'
-import { AppConfig } from '../config'
-import { JwrPayload } from './types'
+import { AppConfig } from '../../config'
+import { JwrPayload } from '../types'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
