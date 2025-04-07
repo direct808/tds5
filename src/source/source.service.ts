2d78d92f8f4adccb83b64689480f29d85a117b37
Larin Sergey <LarinSS@fsk.ru>
2025-04-07 20:24:30 +0300

    Source

--- src/source/source.service.ts
+++ src/source/source.service.ts
@@ -1,11 +1,22 @@
 import { SourceRepository } from './source.repository'
-import { Injectable } from '@nestjs/common'
+import { ConflictException, Injectable } from '@nestjs/common'
+
+type CreateArgs = {
+  name: string
+  userId: string
+}
 
 @Injectable()
 export class SourceService {
   constructor(private readonly repository: SourceRepository) {}
 
-  public async create(name: string): Promise<void> {
-    await this.repository.create(name)
+  public async create(args: CreateArgs): Promise<void> {
+    const existsSource = await this.repository.getByName(args.name, args.userId)
+
+    if (existsSource) {
+      throw new ConflictException('Источник с таким именем уже существует')
+    }
+
+    await this.repository.create(args)
   }
 }
