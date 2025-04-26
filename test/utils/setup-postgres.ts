a3ab01026f2bdd7cf5e72b24bc615475c3b75e11
Larin Sergey <direct808@yandex.ru>
2025-04-26 20:28:23 +0300

    Change postgresql version

--- test/utils/setup-postgres.ts
+++ test/utils/setup-postgres.ts
@@ -2,7 +2,7 @@ import { PostgreSqlContainer } from '@testcontainers/postgresql'
 import * as process from 'node:process'
 
 export async function createTestDataSource() {
-  const container = await new PostgreSqlContainer('postgres:17.2').start()
+  const container = await new PostgreSqlContainer().start()
 
   process.env.DB_HOST = container.getHost()
   process.env.DB_PORT = container.getPort() + ''
