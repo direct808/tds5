d5c617a2df4a2231c647f8324bc66f3074ec97e4
Larin Sergey <LarinSS@fsk.ru>
2025-04-26 16:17:08 +0300

    Offer tests

--- src/offer/offer.service.ts
+++ src/offer/offer.service.ts
@@ -38,7 +38,10 @@ export class OfferService {
     private readonly repository: OfferRepository,
     private readonly networkRepository: AffiliateNetworkRepository,
   ) {}
-
+  /**
+   * Создание партнерской сети
+   * @param args
+   */
   public async create(args: CreateArgs): Promise<void> {
     await checkUniqueNameForCreate(this.repository, args)
 
@@ -46,7 +49,10 @@ export class OfferService {
 
     await this.repository.create(args)
   }
-
+  /**
+   * Обновление партнерский сети
+   * @param args
+   */
   public async update(args: UpdatedArgs): Promise<void> {
     await ensureEntityExists(this.repository, args)
 
@@ -61,17 +67,29 @@ export class OfferService {
 
     await this.repository.update(args.id, args)
   }
-
+  /**
+   * Список партнерских сетей
+   * @param userId
+   */
   public async getList(userId: string): Promise<Offer[]> {
     return this.repository.getListByUserId(userId)
   }
-
+  /**
+   * Удаление партнерской сети
+   * @param args
+   */
   public async delete(args: DeleteArgs): Promise<void> {
     await ensureEntityExists(this.repository, args)
 
     await this.repository.delete(args.id)
   }
-
+  /**
+   * Проверяет существование Affiliate Network для пользователя.
+   *
+   * Если `affiliateNetworkId` отсутствует — метод ничего не делает.
+   *
+   * @param args Данные для проверки сети.
+   */
   private async ensureNetworkExists(args: EnsureNetworkExistsArgs) {
     if (!args.affiliateNetworkId) {
       return
