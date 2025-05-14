57efc4e658d5bba590007334c46f1bd671126337
Larin Sergey <LarinSS@fsk.ru>
2025-05-14 16:53:30 +0300

    dockerfile

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:24-alpine as builder
+
+WORKDIR /app
+COPY package.json yarn.lock ./
+RUN yarn
+COPY . .
+
+RUN yarn run build
+
+FROM node:24-alpine
+
+WORKDIR /app
+COPY package.json yarn.lock ./
+RUN yarn install --prod
+
+COPY --from=builder /app/dist ./dist
+
+CMD [ "yarn", "run", "start:prod" ]
--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,26 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { configureApp } from './utils/configure-app'
+import { AppConfig } from './config'
+import { Logger } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   configureApp(app)
 
-  const config = new DocumentBuilder()
+  const config = app.get(AppConfig)
+  const logger = new Logger()
+
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('TDS 5')
     .setDescription('Трекер веб-аналитики и отслеживания траффика')
     .setVersion('1.0')
     .build()
-  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('api', app, documentFactory, { swaggerOptions: {} })
 
-  await app.listen(3300)
+  await app.listen(config.port)
+  logger.log('Server is listening on port: ' + config.port)
 }
 
 void bootstrap()
