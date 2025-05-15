00c8f05f0c16e9bae95f7dc6426293d4c0b0d8e8
Larin Sergey <LarinSS@fsk.ru>
2025-05-15 17:25:10 +0300

    tds5-13 Simplify module of campaigns

--- src/campaign/campaign.module.ts
+++ src/campaign/campaign.module.ts
@@ -4,12 +4,11 @@ import { CommonCampaignService } from './common-campaign.service'
 import { CampaignRepository } from './campaign.repository'
 import { CreateCampaignService } from './create-campaign.service'
 import { UpdateCampaignService } from './update-campaign.service'
-import { StreamModule } from './stream/stream.module'
 import { SourceModule } from '../source/source.module'
 
 @Module({
   controllers: [CampaignController],
-  imports: [StreamModule, SourceModule],
+  imports: [SourceModule],
   providers: [
     CommonCampaignService,
     CampaignRepository,
