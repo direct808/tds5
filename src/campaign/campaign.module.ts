eb8fe6d246aa759737f1a719c50e51785bd32f9c
Larin Sergey <direct808@yandex.ru>
2025-05-15 17:33:27 +0300

    tds5-13 Simplify module of campaigns (#14)

--- src/campaign/campaign.module.ts
+++ src/campaign/campaign.module.ts
@@ -4,17 +4,34 @@ import { CommonCampaignService } from './common-campaign.service'
 import { CampaignRepository } from './campaign.repository'
 import { CreateCampaignService } from './create-campaign.service'
 import { UpdateCampaignService } from './update-campaign.service'
-import { StreamModule } from './stream/stream.module'
 import { SourceModule } from '../source/source.module'
+import { UpdateStreamService } from './stream/update-stream.service'
+import { CreateStreamOfferService } from './stream-offer/create-stream-offer.service'
+import { CreateStreamService } from './stream/create-stream.service'
+import { CommonStreamService } from './stream/common-stream.service'
+import { StreamRepository } from './stream/stream.repository'
+import { UpdateStreamOfferService } from './stream-offer/update-stream-offer.service'
+import { StreamOfferRepository } from './stream-offer/stream-offer.repository'
+import { CommonStreamOfferService } from './stream-offer/common-stream-offer.service'
+import { OfferRepository } from '../offer/offer.repository'
 
 @Module({
   controllers: [CampaignController],
-  imports: [StreamModule, SourceModule],
+  imports: [SourceModule],
   providers: [
     CommonCampaignService,
     CampaignRepository,
     CreateCampaignService,
     UpdateCampaignService,
+    UpdateStreamService,
+    CreateStreamOfferService,
+    CreateStreamService,
+    CommonStreamService,
+    StreamRepository,
+    UpdateStreamOfferService,
+    StreamOfferRepository,
+    CommonStreamOfferService,
+    OfferRepository,
   ],
   exports: [CampaignRepository],
 })
