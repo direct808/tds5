60cc30000339eae06e87721d206509a0d9052c36
Larin Sergey <LarinSS@fsk.ru>
2025-05-15 17:32:04 +0300

    tds5-13 Simplify module of campaigns

--- src/campaign/campaign.module.ts
+++ src/campaign/campaign.module.ts
@@ -5,6 +5,15 @@ import { CampaignRepository } from './campaign.repository'
 import { CreateCampaignService } from './create-campaign.service'
 import { UpdateCampaignService } from './update-campaign.service'
 import { SourceModule } from '../source/source.module'
+import { UpdateStreamService } from './stream/update-stream.service'
+import { CreateStreamOfferService } from './stream-offer/create-stream-offer.service'
+import { CreateStreamService } from './stream/create-stream.service'
+import { CommonStreamService } from './stream/common-stream.service'
+import { StreamRepository } from './stream/stream.repository'
+import { UpdateStreamOfferService } from './stream-offer/update-stream-offer.service'
+import { StreamOfferRepository } from './stream-offer/stream-offer.repository'
+import { CommonStreamOfferService } from './stream-offer/common-stream-offer.service'
+import { OfferRepository } from '../offer/offer.repository'
 
 @Module({
   controllers: [CampaignController],
@@ -14,6 +23,15 @@ import { SourceModule } from '../source/source.module'
     CampaignRepository,
     CreateCampaignService,
     UpdateCampaignService,
+    UpdateStreamService,
+    CreateStreamOfferService,
+    CreateStreamService,
+    CommonStreamService,
+    StreamRepository,
+    UpdateStreamOfferService,
+    StreamOfferRepository,
+    CommonStreamOfferService,
+    OfferRepository,
   ],
   exports: [CampaignRepository],
 })
