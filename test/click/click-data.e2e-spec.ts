1c52f5b96b14e2625d8d6f0a86fe0a45e81ba2c3
Larin Sergey <direct808@yandex.ru>
2025-06-21 21:39:14 +0300

    Visitor id (#33)

--- test/click/click-data.e2e-spec.ts
+++ test/click/click-data.e2e-spec.ts
@@ -35,97 +35,97 @@ describe('Click-data (e2e)', () => {
     await loadUserFixtures(dataSource)
   })
 
-  describe('Schema type action', () => {
-    it('type TO_CAMPAIGN', async () => {
-      const res = await CampaignBuilder.create()
-        .name('Test campaign 1')
-        .code(code)
-        .userId(userId)
-        .createSource((source) => {
-          source.name('Source 1').userId(userId)
-        })
-        .addStreamTypeOffers((stream) => {
-          stream.name('Stream 1').addOffer((of) => {
-            of.percent(100).createOffer((off) => {
-              off
-                .name('Offer 1')
-                .url(redirectUrl)
-                .userId(userId)
-                .createAffiliateNetwork((an) => {
-                  an.name('Affiliate network 1').userId(userId)
-                })
-            })
+  it('Checks full click data values', async () => {
+    const existsVisitorId = 'abc123'
+    const res = await CampaignBuilder.create()
+      .name('Test campaign 1')
+      .code(code)
+      .userId(userId)
+      .createSource((source) => {
+        source.name('Source 1').userId(userId)
+      })
+      .addStreamTypeOffers((stream) => {
+        stream.name('Stream 1').addOffer((of) => {
+          of.percent(100).createOffer((off) => {
+            off
+              .name('Offer 1')
+              .url(redirectUrl)
+              .userId(userId)
+              .createAffiliateNetwork((an) => {
+                an.name('Affiliate network 1').userId(userId)
+              })
           })
         })
-        .save(dataSource)
-
-      const q = new URLSearchParams()
-      q.append('cost', '5.3496876')
-      q.append('ad_campaign_id', 'ad Campaign Id')
-      q.append('creative_id', 'creative Id')
-      q.append('external_id', 'external_id')
-      q.append('extra_param_1', 'extraParam1')
-      q.append('extra_param_2', 'extraParam2')
-      q.append('sub_id_1', 'sub_id_1 value')
-      q.append('sub_id_2', 'sub_id_2 value')
-      q.append('keyword', 'keyword')
-      q.append('source', 'source value')
-
-      const response = await request(app.getHttpServer())
-        .get(`/${code}?${q.toString()}`)
-        .set({
-          'User-Agent': userAgent,
-          Referer: 'Referer value',
-        })
-        .expect(302)
-
-      expect(response.headers.location).toBe(redirectUrl)
-
-      const clicks = await clickRepo.getByCampaignId(res.id)
-      const offer = res.streams[0].streamOffers![0].offer!
-
-      expect(clicks.length).toBe(1)
-
-      expect(clicks[0]).toEqual({
-        adCampaignId: 'ad Campaign Id',
-        affiliateNetworkId: offer.affiliateNetwork!.id,
-        browser: 'Chrome',
-        browserVersion: '137.0.0.0',
-        campaignId: res.id,
-        country: null,
-        city: null,
-        region: null,
-        cost: '5.35',
-        createdAt: clicks[0].createdAt,
-        creativeId: 'creative Id',
-        destination: redirectUrl,
-        deviceModel: null,
-        deviceType: null,
-        externalId: 'external_id',
-        extraParam1: 'extraParam1',
-        extraParam2: 'extraParam2',
-        id: clicks[0].id,
-        ip: clicks[0].ip,
-        isBot: null,
-        isProxy: null,
-        isUniqueCampaign: null,
-        isUniqueGlobal: null,
-        isUniqueStream: null,
-        keyword: 'keyword',
-        language: null,
-        offerId: offer.id,
-        os: 'Windows',
-        osVersion: '10',
-        previousCampaignId: null,
-        referer: 'Referer value',
-        source: 'source value',
-        streamId: clicks[0].streamId,
-        subId1: 'sub_id_1 value',
-        subId2: 'sub_id_2 value',
-        trafficSourceId: res.sourceId,
-        userAgent: userAgent,
-        visitorId: clicks[0].visitorId,
       })
+      .save(dataSource)
+
+    const q = new URLSearchParams()
+    q.append('cost', '5.3496876')
+    q.append('ad_campaign_id', 'ad Campaign Id')
+    q.append('creative_id', 'creative Id')
+    q.append('external_id', 'external_id')
+    q.append('extra_param_1', 'extraParam1')
+    q.append('extra_param_2', 'extraParam2')
+    q.append('sub_id_1', 'sub_id_1 value')
+    q.append('sub_id_2', 'sub_id_2 value')
+    q.append('keyword', 'keyword')
+    q.append('source', 'source value')
+
+    const response = await request(app.getHttpServer())
+      .get(`/${code}?${q.toString()}`)
+      .set({
+        'User-Agent': userAgent,
+        Referer: 'Referer value',
+      })
+      .set('Cookie', ['visitorId=' + existsVisitorId])
+      .expect(302)
+
+    expect(response.headers.location).toBe(redirectUrl)
+
+    const clicks = await clickRepo.getByCampaignId(res.id)
+    const offer = res.streams[0].streamOffers![0].offer!
+
+    expect(clicks.length).toBe(1)
+
+    expect(clicks[0]).toEqual({
+      id: clicks[0].id,
+      visitorId: existsVisitorId,
+      adCampaignId: 'ad Campaign Id',
+      affiliateNetworkId: offer.affiliateNetwork!.id,
+      browser: 'Chrome',
+      browserVersion: '137.0.0.0',
+      campaignId: res.id,
+      country: null,
+      city: null,
+      region: null,
+      cost: '5.35',
+      createdAt: clicks[0].createdAt,
+      creativeId: 'creative Id',
+      destination: redirectUrl,
+      deviceModel: null,
+      deviceType: null,
+      externalId: 'external_id',
+      extraParam1: 'extraParam1',
+      extraParam2: 'extraParam2',
+      ip: clicks[0].ip,
+      isBot: null,
+      isProxy: null,
+      isUniqueCampaign: null,
+      isUniqueGlobal: null,
+      isUniqueStream: null,
+      keyword: 'keyword',
+      language: null,
+      offerId: offer.id,
+      os: 'Windows',
+      osVersion: '10',
+      previousCampaignId: null,
+      referer: 'Referer value',
+      source: 'source value',
+      streamId: clicks[0].streamId,
+      subId1: 'sub_id_1 value',
+      subId2: 'sub_id_2 value',
+      trafficSourceId: res.sourceId,
+      userAgent: userAgent,
     })
   })
 })
---
+++ test/utils/campaign-builder-facades/create-campaign-content.ts
@@ -0,0 +1,29 @@
+import { CampaignBuilder } from '../../../src/utils/entity-builder/campaign-builder'
+import { StreamActionType } from '../../../src/campaign/entity/stream.entity'
+import { DataSource } from 'typeorm'
+import { faker } from '@faker-js/faker'
+import { Campaign } from '../../../src/campaign/entity/campaign.entity'
+
+type CreateCampaignContentArgs = {
+  dataSource: DataSource
+  userId: string
+  content?: string
+}
+
+export function createCampaignContent({
+  dataSource,
+  userId,
+  content = faker.string.alphanumeric({ length: 6 }),
+}: CreateCampaignContentArgs): Promise<Campaign> {
+  return CampaignBuilder.create()
+    .name(faker.company.name())
+    .code(faker.string.alphanumeric({ length: 6 }))
+    .userId(userId)
+    .addStreamTypeAction((stream) => {
+      stream
+        .name(faker.company.name())
+        .type(StreamActionType.SHOW_TEXT)
+        .content(content)
+    })
+    .save(dataSource)
+}
