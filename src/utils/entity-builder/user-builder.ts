a4c2676a5621ce25ae3582dee0eae3ebd7b0a409
Larin Sergey <direct808@yandex.ru>
2025-06-22 10:33:15 +0300

    User builder (#34)
    * user-builder
    
    * user-builder
    
    ---------
    
    Co-authored-by: Larin Sergey <LarinSS@fsk.ru>

---
+++ src/utils/entity-builder/user-builder.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm'
+import { User } from '../../user/user.entity'
+
+export class UserBuilder {
+  private fields: Partial<User> = {}
+
+  static create() {
+    return new this()
+  }
+
+  email(email: string) {
+    this.fields.email = email
+    return this
+  }
+
+  password(password: string) {
+    this.fields.password = password
+    return this
+  }
+
+  async save(ds: DataSource): Promise<User> {
+    return ds.getRepository(User).save(this.fields)
+  }
+}
