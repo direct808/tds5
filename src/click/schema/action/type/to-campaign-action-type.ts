fe2e6f1e020f9eaf1fec4bc2c5dc6ad4b59ad8c5
Larin Sergey <LarinSS@fsk.ru>
2025-07-10 22:33:03 +0300

    To campaign refactoring

--- src/click/schema/action/type/to-campaign-action-type.ts
+++ src/click/schema/action/type/to-campaign-action-type.ts
@@ -1,31 +1,21 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { ActionType, ClickContext, StreamResponse } from '@/click/types'
-import { ClickService } from '@/click/click.service'
 import { Stream } from '@/campaign/entity/stream.entity'
 
 @Injectable()
 export class ToCampaignActionType implements ActionType {
-  constructor(
-    @Inject(forwardRef(() => ClickService))
-    private readonly clickService: ClickService,
-  ) {}
-
   async handle(
     { actionCampaign }: Stream,
-    cRequest: ClickContext,
+    { clickData }: ClickContext,
   ): Promise<StreamResponse> {
     if (!actionCampaign) {
       throw new Error('No actionCampaign')
     }
-    const nextClickData = structuredClone(cRequest.clickData)
 
-    nextClickData.previousCampaignId = cRequest.clickData.campaignId
-    cRequest.clickData.destination = actionCampaign.name
+    clickData.destination = actionCampaign.name
 
-    return this.clickService.getStreamResponse({
-      ...cRequest,
-      code: actionCampaign.code,
-      clickData: nextClickData,
-    })
+    return {
+      campaignCode: actionCampaign.code,
+    }
   }
 }
