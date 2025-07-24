a2142dba1cea71cb188128bfdb8a70ef312b0321
Larin Sergey <direct808@yandex.ru>
2025-07-24 10:16:31 +0300

    Kysely (#57)
    * Kysely
    
    * Kysely
    
    ---------
    
    Co-authored-by: Larin Sergey <LarinSS@fsk.ru>

--- src/click/click.repository.ts
+++ src/click/click.repository.ts
@@ -2,12 +2,18 @@ import { Injectable } from '@nestjs/common'
 import { Click } from './click.entity'
 import { DataSource } from 'typeorm'
 import { IClick } from './click'
+import { Kysely } from 'kysely'
+import { InjectKysely } from 'nestjs-kysely'
+import { DB } from '@/@types/db'
 
 @Injectable()
 export class ClickRepository {
   private readonly repository = this.dataSource.getRepository(Click)
 
-  constructor(private readonly dataSource: DataSource) {}
+  constructor(
+    private readonly dataSource: DataSource,
+    @InjectKysely() private readonly db: Kysely<DB>,
+  ) {}
 
   public async add(data: Partial<IClick>): Promise<void> {
     await this.repository.insert(data)
