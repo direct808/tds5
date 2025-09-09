26443d77ce5d882364d05af0faacb377029a0f22
Larin Sergey <direct808@yandex.ru>
2025-09-09 10:40:04 +0300

    Pino

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,36 @@
 import { NestFactory } from '@nestjs/core'
-import { Logger } from '@nestjs/common'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 import { configureApp } from './utils/configure-app'
 import { AppConfig } from './config/app-config.service'
+import { Logger } from 'nestjs-pino'
+import pino from 'pino'
+import { window } from 'rxjs'
+
+// const transport = pino.transport({
+//   target: 'pino-loki',
+//   options: {
+//     batching: true,
+//     interval: 5,
+//     host: 'http://localhost:3100',
+//     labels: { app: 'nest-service' },
+//   },
+// })
+//
+// export const logger = pino(transport)
+
+// setInterval(function () {
+//   logger.info('11111111111111111111111111111111111111111111111111111111')
+// }, 50)
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, { bufferLogs: true })
   configureApp(app)
 
+  const logger = app.get(Logger)
+  app.useLogger(logger)
+
   const config = app.get(AppConfig)
-  const logger = new Logger()
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('TDS 5')
