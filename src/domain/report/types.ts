269a32d8a5020eb02fe0c0e1acb2aa06fffd5a1e
Larin Sergey <direct808@yandex.ru>
2025-12-25 14:14:58 +0300

    Pagination

--- src/domain/report/types.ts
+++ src/domain/report/types.ts
@@ -2,10 +2,7 @@ type Row = {
   [key: string]: string | number
 }
 
-export type ReportResult = {
-  rows: Row[]
-  summary: Row[]
-}
+
 
 export type Formula = { formula: string; decimals?: number }
 export type FormulaRecord = Record<string, Formula>
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,3 @@
-export type Nullable<T> = {
-  [P in keyof T]: T[P] | undefined | null
-}
+
 
 export type MaybePromise<T> = T | Promise<T>
