135cb40dcbf7135d318740efb6275a0781ee438d
Larin Sergey <direct808@yandex.ru>
2026-03-12 12:50:13 +0300

    Docker build (#119)

--- apps/backend/src/domain/geo-ip/mmdb-geo-ip.service.ts
+++ apps/backend/src/domain/geo-ip/mmdb-geo-ip.service.ts
@@ -1,27 +1,25 @@
 import { Injectable } from '@nestjs/common'
-import * as fs from 'node:fs'
 import { GeoIpService } from './types'
-import { CityResponse, Reader } from 'mmdb-lib'
 
 @Injectable()
 export class MmdbGeoIpService implements GeoIpService {
-  private readonly reader: Reader<CityResponse>
+  // private readonly reader: Reader<CityResponse>
 
   constructor() {
-    const db = fs.readFileSync('./geo-ip.mmdb')
-    this.reader = new Reader<CityResponse>(db)
+    // const db = fs.readFileSync('./geo-ip.mmdb')
+    // this.reader = new Reader<CityResponse>(db)
   }
 
-  public get: GeoIpService['get'] = (ip) => {
-    const result = this.reader.get(ip)
-    if (!result) {
-      return
-    }
+  public get: GeoIpService['get'] = () => {
+    // const result = this.reader.get(ip)
+    // if (!result) {
+    return undefined
+    // }
 
-    return {
-      country: result.country?.iso_code,
-      region: result.subdivisions?.[0]?.names.en,
-      city: result.city?.names.en,
-    }
+    // return {
+    //   country: result.country?.iso_code,
+    //   region: result.subdivisions?.[0]?.names.en,
+    //   city: result.city?.names.en,
+    // }
   }
 }
