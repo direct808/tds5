cce7f9ef710237560b167c9e3e6ecc7138638f8b
Larin Sergey <direct808@yandex.ru>
2026-03-14 17:54:21 +0300

    knip

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,4 @@
+import 'source-map-support/register'
 import { NestFactory } from '@nestjs/core'
 import { Logger } from '@nestjs/common'
 import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger'
